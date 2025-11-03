6a0f723be0ab5b5d4927766be5a4d1acb7a8a914
immortal-music <emoji2292299@gmail.com>
2025-11-03 13:59:13 +0630

    Add files via upload

---
+++ maythusharmusic/plugins/stop.py
@@ -0,0 +1,17 @@
+from pyrogram import filters, types
+
+from maythusharmusic import cailin, app, db, lang
+from maythusharmusic.helpers import can_manage_vc
+
+
+@app.on_message(filters.command(["end", "stop"]) & filters.group & ~app.bl_users)
+@lang.language()
+@can_manage_vc
+async def _stop(_, m: types.Message):
+    if len(m.command) > 1:
+        return
+    if not await db.get_call(m.chat.id):
+        return await m.reply_text(m.lang["not_playing"])
+
+    await cailin.stop(m.chat.id)
+    await m.reply_text(m.lang["play_stopped"].format(m.from_user.mention))
